97c9f4b55633b1c340ad58313bc5db39f2a73355
Jaypee <Jaypee.Vilador@gmail.com>
2025-05-11 06:04:57 +0800

    Add navigation bar and style

---
+++ Header.jsx
@@ -0,0 +1,13 @@
+export default function Header () {
+return	(
+	<header>
+		<img src="react-logo.png" width="40px" alt="React logo" />
+		<nav>
+			<ul>
+			<li>Pricing</li>
+			<li>About</li>
+			<li>Contact</li>
+			</ul>
+		</nav>
+    </header>
+	)}
--- index.jsx
+++ index.jsx
@@ -1,15 +1,12 @@
 import {createRoot} from "react-dom/client"
+import Footer from "./Footer"
+import Header from "./Header"
+import MainContent from "./MainContent"
 const root = createRoot(document.getElementById("root"));
 root.render(
-    <main>
-        <img src="react-logo.png" style={{width:"40px"}} alt="React logo" />
-        <h1>Fun facts about React</h1>
-            <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100k stars on Github</li>
-            <li>Is maintained by Meta</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
-    </main>
+    <div>
+        <Header />
+        <MainContent />
+        <Footer />
+    </div>
 )
